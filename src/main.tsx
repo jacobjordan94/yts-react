43b105e37392b1d5fc5370fc9614a806c6754fe6
Jacob Jordan <jacobjordan94@live.com>
2025-10-05 13:52:47 -0500

    add react-router

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+const Root = () => import.meta.env.PROD ? <App /> : <StrictMode><App /></StrictMode>;
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Root />
 )
